75e31e59886aa7f5da61690a8e5ba65f55356453
kirilikl <kirill.ahremenko@gmail.com>
2024-03-20 16:48:47 +0300

    first js attempt

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let newGame = document.querySelector("#newGame")
 
+console.log(newGame)
+
+newGame.addEventListener("click", () => {
+	console.log("kekw")
+	document.getElementById("main").style.display = "none"
+})
