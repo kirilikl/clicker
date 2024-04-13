64ad50cb125a4369a70f9ba624e34de6c08a51b3
kirilikl <kirill.ahremenko@gmail.com>
2024-04-13 22:46:55 +0300

    v 0.11

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ let menu = document.querySelector(".menu")
 let game = document.querySelector(".game")
 let cookie = document.querySelector(".cookie")
 let scoreHTML = document.querySelector(".score")
+let shop1 = document.querySelector(".shop1")
+let itemcount = document.querySelector(".itemcount")
 
 let score = 0
+let autoClick = 0
 
 newGameBut.addEventListener("click", () => {
 	menu.style.display = "none"
 	game.style.display = "flex"
+})
+
+shop1.addEventListener("click", () => {
+	if (score >= 10) {
+		score -= 10
+		autoClick += 10
+		scoreHTML.innerHTML = score
+	}
+	itemcount.innerHTML = autoClick / 10
 })
 
 cookie.addEventListener("click", () => {
@@ -18,12 +30,17 @@ cookie.addEventListener("click", () => {
 	anime({
 		targets: ".cookie",
 		scale: 1.08,
-		duration: 200,
-	}).complete(
+		duration: 100,
+	}).finished.then(() => {
 		anime({
 			targets: ".cookie",
 			scale: 1.0,
 			duration: 100,
 		})
-	)
+	})
 })
+
+setInterval(() => {
+	score += autoClick
+	scoreHTML.innerHTML = score
+}, 1000)
